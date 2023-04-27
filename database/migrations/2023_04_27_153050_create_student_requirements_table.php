beb94ce3633c03ff602924102931e7018310aee2
Kaizerrr <jmc6740@gmail.com>
2023-04-28 00:12:32 +0800

    first commit

---
+++ database/migrations/2023_04_27_153050_create_student_requirements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use app\Models\Requirement;
+use app\Models\Student;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_requirements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('requirement_id')->constrained('requirements');
+            $table->foreignId('student_id')->constrained('students');
+            $table->tinyInteger('submitted');
+            $table->longText('file_name');
+            $table->string('filetype');
+            $table->binary('filedata');
+            $table->integer('filesize');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_requirements');
+    }
+};
